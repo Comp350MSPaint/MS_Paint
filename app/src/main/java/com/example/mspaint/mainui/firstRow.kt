a487cbf82cceaab392859ad6f98f1e31dca43b78
koheiitokazu <kohei.itokazu077@myci.csuci.edu>
2024-04-30 23:49:18 -0700

    palette images fit flush with palette buttons

--- app/src/main/java/com/example/mspaint/mainui/firstRow.kt
+++ app/src/main/java/com/example/mspaint/mainui/firstRow.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
@@ -68,6 +69,7 @@ fun firstRow(
         }
         Box(
             modifier = Modifier
+                .padding(14.dp)
                 .weight(5f)
                 .align(Alignment.CenterVertically)
         ) {
