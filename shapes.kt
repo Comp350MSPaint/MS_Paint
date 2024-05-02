4e7bf169dd82190690cdc44834dc797ddd8c65c3
noahsoriano <123587272+noahsoriano@users.noreply.github.com>
2024-05-02 15:28:39 -0700

    Add files via upload

--- shapes.kt
+++ shapes.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,12 +16,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mspaint.R
@@ -37,19 +43,23 @@ fun ShapesFunc (
     onToggleCircle: (Boolean) -> Unit,
     onToggleLine: (Boolean) -> Unit,
 
-
+    slider: @Composable () -> Unit
     ) {
+    var triangleToggled by remember { mutableStateOf(false) }
+    var squareToggled by remember { mutableStateOf(false) }
+    var circleToggled by remember { mutableStateOf(false) }
+    var lineToggled by remember { mutableStateOf(false) }
+
+
     Column(
         modifier = Modifier.background(Color.Black)
-            .height(100.dp)
-            .width(245.dp)
+            .fillMaxSize()
             .background(Color.Yellow)
             .border(width = 3.dp, color = Color.Black)
     ){
         Row(
             modifier = Modifier
-                .width(245.dp)
-                .height(75.dp)
+
         ){
             Box(
                 modifier = Modifier
@@ -59,7 +69,12 @@ fun ShapesFunc (
                     onClick = {onToggleTriangle(true)
                         onToggleSquare(false)
                         onToggleCircle(false)
-                        onToggleLine(false)},
+                        onToggleLine(false)
+                        triangleToggled = true
+                        squareToggled = false
+                        circleToggled = false
+                        lineToggled = false
+                    },
 
                     shape = RectangleShape,
                     //colors = ButtonDefaults.buttonColors(Red),
@@ -70,6 +85,7 @@ fun ShapesFunc (
                             color = Color.Black)
                 )
                 {
+
 
                 }
                 Image(
@@ -82,7 +98,13 @@ fun ShapesFunc (
                             color = Color.Black),
                 contentScale = ContentScale.FillBounds // Adjust cont
                 )
-
+                if (triangleToggled){
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .align(Alignment.Center)
+                        .background(Color.Yellow)
+                )}
             }
             Box(
                 modifier = Modifier
@@ -92,7 +114,12 @@ fun ShapesFunc (
                     onClick = {onToggleTriangle(false)
                         onToggleSquare(false)
                         onToggleCircle(true)
-                        onToggleLine(false)},
+                        onToggleLine(false)
+                        triangleToggled = false
+                        squareToggled = false
+                        circleToggled = true
+                        lineToggled = false
+                    },
 
                     shape = RectangleShape,
                     //colors = ButtonDefaults.buttonColors(Red),
@@ -115,6 +142,13 @@ fun ShapesFunc (
                             color = Color.Black),
                     contentScale = ContentScale.FillBounds // Adjust cont
                 )
+                if (circleToggled){
+                    Box(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .align(Alignment.Center)
+                            .background(Color.Yellow)
+                    )}
 
             }
             Box(
@@ -125,7 +159,12 @@ fun ShapesFunc (
                     onClick = {onToggleTriangle(false)
                         onToggleSquare(true)
                         onToggleCircle(false)
-                        onToggleLine(false)},
+                        onToggleLine(false)
+                        triangleToggled = false
+                        squareToggled = true
+                        circleToggled = false
+                        lineToggled = false
+                    },
 
                     shape = RectangleShape,
                     //colors = ButtonDefaults.buttonColors(Red),
@@ -148,6 +187,13 @@ fun ShapesFunc (
                             color = Color.Black),
                     contentScale = ContentScale.FillBounds // Adjust cont
                 )
+                if (squareToggled){
+                    Box(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .align(Alignment.Center)
+                            .background(Color.Yellow)
+                    )}
 
             }
             Box(
@@ -158,7 +204,12 @@ fun ShapesFunc (
                     onClick = {onToggleTriangle(false)
                         onToggleSquare(false)
                         onToggleCircle(false)
-                        onToggleLine(true)},
+                        onToggleLine(true)
+                        triangleToggled = false
+                        squareToggled = false
+                        circleToggled = false
+                        lineToggled = true
+                    },
 
                     shape = RectangleShape,
                     //colors = ButtonDefaults.buttonColors(Red),
@@ -181,28 +232,40 @@ fun ShapesFunc (
                             color = Color.Black),
                     contentScale = ContentScale.FillBounds // Adjust cont
                 )
+                if (lineToggled){
+                    Box(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .align(Alignment.Center)
+                            .background(Color.Yellow)
+                    )}
 
             }
 
         }
         Row(
             modifier = Modifier
-                .width(245.dp)
-                .height(40.dp)
-                .padding(start = 16.dp), // Adjust padding as needed
-            verticalAlignment = Alignment.Top // Align text to the top of the row
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally), // Adjust padding as needed
+            //verticalAlignment = Alignment.Top // Align text to the top of the row
         ) {
-            Text(
-                text = "t \n",
-                color = Color.Black,
-                fontSize = 20.sp, // Adjust the text size here
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth() // Fill the entire width of the row
-                    .alignByBaseline() // Align text to the baseline of the row
-            )
+                    .fillMaxSize()
+                    .align(Alignment.CenterVertically)
+            ) {
+                slider()
+            }
         }
 
 
 
     }
+}
+
+
+@Preview
+@Composable
+fun ShapesPreview() {
+    MainScreen()
 }
